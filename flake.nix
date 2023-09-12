d161198dc6f14e01bc65e275267dea86d99216ea
MalekiRe <pocmalek@gmail.com>
2023-09-11 17:45:07 -0700

    modified flake with cmake

--- flake.nix
+++ flake.nix
@@ -39,6 +39,8 @@
             pkgs.cargo-deny
             pkgs.cargo-expand
             pkgs.cargo-binutils
+            # cmake for openxr
+            pkgs.cmake
           ];
 
           # see https://github.com/NixOS/nixpkgs/blob/95b81c96f863ca8911dffcda45d1937efcd66a4b/pkgs/games/jumpy/default.nix#L60C5-L60C38
