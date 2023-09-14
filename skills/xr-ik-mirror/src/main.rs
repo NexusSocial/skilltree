936c06e76903a880f2f4e3c5e5883fc7261d60fc
piper <107778087+awtterpip@users.noreply.github.com>
2023-09-13 23:39:11 -0500

    i got mirrors working and updated openxr-6dof to use the newest bevy_openxr (#21)
    * made mirrors working and fixed a few things
    
    * deleted unncessary file
    
    * documented things
    
    * formatted code cuz i forgor
    
    * fixed IK partially and added bevy_mirror_dwelling_simluator
    
    * fmt
    
    * cargo fix
    
    ---------
    
    Co-authored-by: MalekiRe <pocmalek@gmail.com>

--- skills/xr-ik-mirror/src/main.rs
+++ skills/xr-ik-mirror/src/main.rs
@@ -1,12 +1,18 @@
+use bevy::core_pipeline::clear_color::ClearColorConfig;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::math::vec3;
+use bevy::prelude::EulerRot::XYZ;
 use bevy::prelude::*;
+use bevy::render::camera::RenderTarget;
+use bevy::render::render_resource::{
+	Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+};
 use bevy::transform::components::Transform;
+
 use bevy_openxr::input::XrInput;
 use bevy_openxr::resources::XrFrameState;
-use bevy_openxr::xr_input::controllers::XrControllerType;
 use bevy_openxr::xr_input::oculus_touch::OculusController;
-use bevy_openxr::xr_input::{OpenXrInput, QuatConv, Vec3Conv};
+use bevy_openxr::xr_input::{QuatConv, Vec3Conv};
 use bevy_openxr::DefaultXrPlugins;
 
 const ASSET_FOLDER: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../../assets/");
@@ -17,12 +23,14 @@ fn main() {
 	info!("Running `openxr-6dof` skill");
 	App::new()
 		.add_plugins(DefaultXrPlugins)
-		.add_plugins(OpenXrInput::new(XrControllerType::OculusTouch))
 		.add_plugins(LogDiagnosticsPlugin::default())
 		.add_plugins(FrameTimeDiagnosticsPlugin)
 		.add_plugins(bevy_mod_inverse_kinematics::InverseKinematicsPlugin)
 		.add_systems(Startup, setup)
-		.add_systems(Update, (hands, setup_ik, head_sync, body_sync))
+		.add_systems(
+			Update,
+			(hands, setup_ik, head_sync, body_sync, true_head_sync),
+		)
 		.run();
 }
 
@@ -33,9 +41,99 @@ pub struct AvatarSetup;
 fn setup(
 	mut commands: Commands,
 	mut meshes: ResMut<Assets<Mesh>>,
+	mut images: ResMut<Assets<Image>>,
 	assets: Res<AssetServer>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+	let bevy_mirror_dwelling_img: Handle<Image> =
+		assets.load(&(ASSET_FOLDER.to_string() + "bevy_mirror_dwelling.png"));
+	commands.spawn(PbrBundle {
+		mesh: meshes.add(shape::Cube::default().into()),
+		material: materials.add(StandardMaterial {
+			base_color_texture: Some(bevy_mirror_dwelling_img),
+			..default()
+		}),
+		transform: Transform::from_xyz(0.0, 2.2, -2.0)
+			.with_scale(Vec3::new(2.0, 0.5, 0.01))
+			.with_rotation(Quat::from_euler(
+				XYZ,
+				180.0_f32.to_radians(),
+				0.0,
+				180.0_f32.to_radians(),
+			)),
+		..default()
+	});
+	let size = Extent3d {
+		width: 512,
+		height: 512,
+		..default()
+	};
+	// This is the texture that will be rendered to.
+	let mut image = Image {
+		texture_descriptor: TextureDescriptor {
+			label: None,
+			size,
+			dimension: TextureDimension::D2,
+			format: TextureFormat::Bgra8UnormSrgb,
+			mip_level_count: 1,
+			sample_count: 1,
+			usage: TextureUsages::TEXTURE_BINDING
+				| TextureUsages::COPY_DST
+				| TextureUsages::RENDER_ATTACHMENT,
+			view_formats: &[],
+		},
+		..default()
+	};
+
+	// fill image.data with zeroes
+	image.resize(size);
+	// image for the mirror
+	let image_handle = images.add(image);
+
+	// material for the mirror
+	let mirror_material_handle = materials.add(StandardMaterial {
+		base_color_texture: Some(image_handle.clone()),
+		reflectance: 0.02,
+		unlit: false,
+		..default()
+	});
+	// the plane displaying the mirrors texture
+	let mirror = commands
+		.spawn(PbrBundle {
+			mesh: meshes.add(Mesh::from(shape::Quad {
+				size: Vec2 { x: 2.0, y: 2.0 },
+				flip: true,
+			})),
+			material: mirror_material_handle,
+			..default()
+		})
+		.id();
+	// camera for mirror
+	let camera = commands
+		.spawn(Camera3dBundle {
+			camera_3d: Camera3d {
+				clear_color: ClearColorConfig::Custom(Color::WHITE),
+				..default()
+			},
+			camera: Camera {
+				// render before the "main pass" camera
+				order: -1,
+				target: RenderTarget::Image(image_handle.clone()),
+				..default()
+			},
+			transform: Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(
+				Quat::from_euler(EulerRot::XYZ, 0.0, 180.0_f32.to_radians(), 0.0),
+			),
+			..default()
+		})
+		.id();
+	commands
+		.spawn(SpatialBundle {
+			transform: Transform::from_xyz(0.0, 1.0, -2.0),
+			..default()
+		})
+		.push_children(&[camera, mirror]);
+
 	// plane
 	commands.spawn(PbrBundle {
 		mesh: meshes.add(shape::Plane::from_size(5.0).into()),
@@ -68,7 +166,7 @@ fn setup(
 		SceneBundle {
 			scene: assets.load(&(ASSET_FOLDER.to_string() + "/malek.gltf#Scene0")),
 			transform: Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(
-				Quat::from_euler(EulerRot::XYZ, 0.0, 180.0_f32.to_radians(), 0.0),
+				Quat::from_euler(EulerRot::XYZ, 0.0, 0.0_f32.to_radians(), 0.0),
 			),
 			..default()
 		},
@@ -89,6 +187,27 @@ pub struct Head;
 pub struct Avatar;
 #[derive(Component)]
 pub struct Hips;
+
+#[derive(Component)]
+pub struct TrueHead;
+
+fn true_head_sync(
+	mut head_query: Query<(&mut Transform, &TrueHead)>,
+	frame_state: Res<XrFrameState>,
+	xr_input: Res<XrInput>,
+) {
+	let mut func = || -> color_eyre::Result<()> {
+		let frame_state = *frame_state.lock().unwrap();
+		let a = xr_input
+			.head
+			.relate(&xr_input.stage, frame_state.predicted_display_time)?;
+		for (mut head, _) in head_query.iter_mut() {
+			head.rotation = a.0.pose.orientation.to_quat();
+		}
+		Ok(())
+	};
+	let _ = func();
+}
 
 fn head_sync(
 	mut head_query: Query<(&mut Transform, &Head)>,
@@ -176,14 +295,14 @@ fn hands(
 						translation: left_controller.0.pose.position.to_vec3(),
 						rotation: left_controller.0.pose.orientation.to_quat(),
 						scale: Default::default(),
-					}
+					};
 				}
 				Hand::Right => {
 					*transform = Transform {
 						translation: right_controller.0.pose.position.to_vec3(),
 						rotation: right_controller.0.pose.orientation.to_quat(),
 						scale: Default::default(),
-					}
+					};
 				}
 			}
 		}
@@ -195,8 +314,8 @@ fn hands(
 
 fn setup_ik(
 	mut commands: Commands,
-	mut meshes: ResMut<Assets<Mesh>>,
-	mut materials: ResMut<Assets<StandardMaterial>>,
+	_meshes: ResMut<Assets<Mesh>>,
+	_materials: ResMut<Assets<StandardMaterial>>,
 	added_query: Query<(Entity, &AvatarSetup)>,
 	children: Query<&Children>,
 	names: Query<&Name>,
@@ -243,21 +362,8 @@ fn setup_ik(
 		};
 		commands.entity(entity).remove::<AvatarSetup>();
 
-		let pole_target = commands
-			.spawn(PbrBundle {
-				transform: Transform::from_xyz(-1.0, 0.4, -0.2),
-				mesh: meshes.add(Mesh::from(shape::UVSphere {
-					radius: 0.05,
-					sectors: 7,
-					stacks: 7,
-				})),
-				material: materials.add(StandardMaterial {
-					base_color: Color::GREEN,
-					..default()
-				}),
-				..default()
-			})
-			.id();
+		commands.entity(hips).insert(Hips);
+		commands.entity(head).insert(TrueHead);
 		let target_entity1 = commands
 			.spawn((TransformBundle::default(), Hand::Right))
 			.id();
@@ -265,7 +371,7 @@ fn setup_ik(
 			.spawn((TransformBundle::default(), Hand::Left))
 			.id();
 		let target_entity3 = commands.spawn((TransformBundle::default(), Head)).id();
-		let hips_entity = commands.spawn((TransformBundle::default(), Hips)).id();
+		let _hips_entity = commands.spawn((TransformBundle::default(), Hips)).id();
 		// Add an IK constraint to the right hand, using the targets that were created earlier.
 		commands
 			.entity(left_hand)
@@ -273,8 +379,8 @@ fn setup_ik(
 				chain_length: 3,
 				iterations: 20,
 				target: target_entity1,
-				pole_target: Some(pole_target),
-				pole_angle: -std::f32::consts::FRAC_PI_2,
+				pole_target: None,
+				pole_angle: std::f32::consts::FRAC_PI_4,
 				enabled: true,
 			});
 		commands
@@ -283,8 +389,8 @@ fn setup_ik(
 				chain_length: 3,
 				iterations: 20,
 				target: target_entity2,
-				pole_target: Some(pole_target),
-				pole_angle: -std::f32::consts::FRAC_PI_2,
+				pole_target: None,
+				pole_angle: -std::f32::consts::FRAC_PI_4,
 				enabled: true,
 			});
 		commands
@@ -293,19 +399,19 @@ fn setup_ik(
 				chain_length: 1,
 				iterations: 20,
 				target: target_entity3,
-				pole_target: Some(pole_target),
-				pole_angle: -std::f32::consts::FRAC_PI_2,
+				pole_target: None,
+				pole_angle: 180_f32.to_radians(),
 				enabled: true,
 			});
-		commands
-			.entity(hips)
-			.insert(bevy_mod_inverse_kinematics::IkConstraint {
-				chain_length: 1,
-				iterations: 20,
-				target: hips_entity,
-				pole_target: Some(pole_target),
-				pole_angle: -std::f32::consts::FRAC_PI_2,
-				enabled: true,
-			});
+		// commands
+		// 	.entity(hips)
+		// 	.insert(bevy_mod_inverse_kinematics::IkConstraint {
+		// 		chain_length: 2,
+		// 		iterations: 20,
+		// 		target: hips_entity,
+		// 		pole_target: None,
+		// 		pole_angle: 180_f32.to_radians(),
+		// 		enabled: true,
+		// 	});
 	}
 }
